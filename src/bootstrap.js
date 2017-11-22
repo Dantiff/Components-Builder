7f8d7bc8b4098fb71784e0ff6cc5490bc5a15b5d
Dantiff <daniel@touchinspiration.com>
2017-11-22 15:44:30 +0300

    Add configs for vue2-google-maps

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -220,6 +220,20 @@ loadSolidGauge(Highcharts);
 // Now you can use Highstock, Highmaps, drilldown and solid gauge.
 Vue.use(VueHighcharts, { Highcharts });
 
+
+import * as VueGoogleMaps from 'vue2-google-maps';
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyBvWE_sIwKbWkiuJQOf8gSk9qzpO96fhfY',
+    libraries: 'places,drawing,visualization', // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+  },
+});
+
+
 export default {
   router,
   i18n,
