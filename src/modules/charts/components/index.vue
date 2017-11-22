d31b5e9170c0a57cc8f28d04219cea8417405687
Dantiff <daniel@touchinspiration.com>
2017-11-22 11:27:48 +0300

    Update packaje.json and upgrate to vuetify 0.17

--- src/modules/charts/components/index.vue
+++ src/modules/charts/components/index.vue
@@ -2,7 +2,12 @@
 <script>
   export default {
     data() {
-      return {};
+      return {
+        highcharts: [
+          { link: { name: 'charts.highcharts.multilinear' }, title: 'Highcharts: Multilinear' },
+          { link: { name: 'charts.highcharts.timeseries' }, title: 'Highcharts: Time Series' },
+        ],
+      };
     },
   };
 </script>
@@ -12,22 +17,29 @@
     <v-container fluid class="quotes">
       <v-slide-y-transition mode="out-in">
         <v-layout column align-left class="container">
-          <blockquote>
-            <router-link :to="{ name: 'charts.highcharts.multilinear' }"> Highcharts: Multilinear </router-link>
-            <footer>
-              <small>
-                <em>&mdash;Carl Dantiff</em>
-              </small>
-            </footer>
-          </blockquote>
-          <blockquote>
-            <router-link :to="{ name: 'charts.highcharts.timeseries' }"> Highcharts: Time Series </router-link>
-            <footer>
-              <small>
-                <em>&mdash;Carl Dantiff</em>
-              </small>
-            </footer>
-          </blockquote>
+          <v-container>
+            <v-layout row wrap>
+              <v-flex xs12 lg5>
+                <v-expansion-panel popout>
+                  <v-expansion-panel-content>
+                    <div slot="header">Highcharts</div>
+                    <v-card v-for="(chart,i) in highcharts" :key="i">
+                      <v-card-text class="grey lighten-3">
+                        <blockquote>
+                          <router-link :to="chart.link"> {{ chart.title }} </router-link>
+                          <footer>
+                            <small>
+                              <em>&mdash;Carl Dantiff</em>
+                            </small>
+                          </footer>
+                        </blockquote>
+                      </v-card-text>
+                    </v-card>
+                  </v-expansion-panel-content>
+                </v-expansion-panel>
+              </v-flex>
+            </v-layout>
+          </v-container>
           <blockquote>
             &#8220;The best performance improvement is the transition from the nonworking state to the working state.&#8221;
             <footer>
