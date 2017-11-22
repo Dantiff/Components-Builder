eca7baf2a12057bd47b1b964f80efe13ec0321bd
Dantiff <daniel@touchinspiration.com>
2017-11-22 11:50:49 +0300

    Update charts index

--- src/modules/charts/components/index.vue
+++ src/modules/charts/components/index.vue
@@ -3,9 +3,23 @@
   export default {
     data() {
       return {
-        highcharts: [
-          { link: { name: 'charts.highcharts.multilinear' }, title: 'Highcharts: Multilinear' },
-          { link: { name: 'charts.highcharts.timeseries' }, title: 'Highcharts: Time Series' },
+        charts: [
+          {
+            title: 'Highcharts',
+            text: 'A es6-based implementation of highcharts',
+            charts: [
+              { link: { name: 'charts.highcharts.multilinear' }, title: 'Highcharts: Multilinear' },
+              { link: { name: 'charts.highcharts.timeseries' }, title: 'Highcharts: Time Series' },
+            ],
+          },
+          {
+            title: 'Chart.js',
+            text: 'A es6-based implementation of chart.js',
+            charts: [
+              { link: { name: 'charts.highcharts.multilinear' }, title: 'Highcharts: Multilinear' },
+              { link: { name: 'charts.highcharts.timeseries' }, title: 'Highcharts: Time Series' },
+            ],
+          },
         ],
       };
     },
@@ -19,14 +33,17 @@
         <v-layout column align-left class="container">
           <v-container>
             <v-layout row wrap>
-              <v-flex xs12 lg5>
+              <v-flex xs12 lg12>
                 <v-expansion-panel popout>
-                  <v-expansion-panel-content>
-                    <div slot="header">Highcharts</div>
-                    <v-card v-for="(chart,i) in highcharts" :key="i">
+                  <v-expansion-panel-content v-for="(chart,i) in charts" :key="i">
+                    <div slot="header">
+                      <h4> {{ chart.title }} </h4>
+                      <p> {{ chart.text }} </p>
+                    </div>
+                    <v-card v-for="(c,i) in chart.charts" :key="i">
                       <v-card-text class="grey lighten-3">
                         <blockquote>
-                          <router-link :to="chart.link"> {{ chart.title }} </router-link>
+                          <router-link :to="c.link"> {{ c.title }} </router-link>
                           <footer>
                             <small>
                               <em>&mdash;Carl Dantiff</em>
@@ -38,16 +55,20 @@
                   </v-expansion-panel-content>
                 </v-expansion-panel>
               </v-flex>
+              <v-flex xs12 lg12>
+                <v-card-text>
+                  <blockquote>
+                    &#8220;The best performance improvement is the transition from the nonworking state to the working state.&#8221;
+                    <footer>
+                      <small>
+                        <em>&mdash;J. Osterhout</em>
+                      </small>
+                    </footer>
+                  </blockquote>
+                </v-card-text>
+              </v-flex>
             </v-layout>
           </v-container>
-          <blockquote>
-            &#8220;The best performance improvement is the transition from the nonworking state to the working state.&#8221;
-            <footer>
-              <small>
-                <em>&mdash;J. Osterhout</em>
-              </small>
-            </footer>
-          </blockquote>
         </v-layout>
       </v-slide-y-transition>
     </v-container>
