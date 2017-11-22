f9417105b22a63a4b4f6239cdaaeb716bf84df1d
Dantiff <daniel@touchinspiration.com>
2017-11-22 16:19:26 +0300

    use personal google api_ket to get location autocomplete

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -225,7 +225,7 @@ import * as VueGoogleMaps from 'vue2-google-maps';
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyBvWE_sIwKbWkiuJQOf8gSk9qzpO96fhfY',
+    key: 'AIzaSyBD-HS_7rOrfHc3O6hgTyBk31rUAnwD78o',
     libraries: 'places,drawing,visualization', // This is required if you use the Autocomplete plugin
     // OR: libraries: 'places,drawing'
     // OR: libraries: 'places,drawing,visualization'
