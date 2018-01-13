7d00db4af70a5a74368a7847c6904a78eb6f0b14
Dantiff <daniel@touchinspiration.com>
2018-01-13 17:04:18 +0300

    Suggestions work

--- src/modules/forms/components/tagInput.vue
+++ src/modules/forms/components/tagInput.vue
@@ -4,15 +4,27 @@
     $validates: true,
     data() {
       return {
-        name: '',
+        selection: '',
         select: null,
-        items: [
-          'Item 1',
-          'Item 2',
-          'Item 3',
-          'Item 4',
-        ],
+        open: false,
+        current: 0,
+        suggestions: ['Intoxicating', 'Life', 'Command', 'Intense', 'Carrier', 'Dominant'],
       };
+    },
+    computed: {
+      // Filtering the suggestion based on the input
+      matches() {
+        // eslint-disable-next-line
+        return this.suggestions.filter((str) => {
+          return str.indexOf(this.selection) >= 0;
+        });
+      },
+      // The flag
+      openSuggestion() {
+        return this.selection !== '' &&
+          this.matches.length !== 0 &&
+          this.open === true;
+      },
     },
     methods: {
       submit() {
@@ -26,22 +38,26 @@
       // When enter pressed on the input
       enter() {
         console.log('entered');
-        // this.selection = this.matches[this.current];
-        // this.open = false;
+        this.selection = this.matches[this.current];
+        this.open = false;
       },
 
       // When up pressed while suggestions are open
       up() {
         console.log('entered up');
-        // if(this.current > 0)
-        //   this.current--;
+        if (this.current > 0) {
+          // eslint-disable-next-line
+          this.current--;
+        }
       },
 
       // When up pressed while suggestions are open
       down() {
         console.log('entered down');
-        // if(this.current < this.matches.length - 1)
-        //   this.current++;
+        if (this.current < this.matches.length - 1) {
+          // eslint-disable-next-line
+          this.current++;
+        }
       },
 
       // For highlighting element
@@ -52,10 +68,10 @@
       // When the user changes input
       change() {
         console.log('changed');
-        // if (this.open == false) {
-        //   this.open = true;
-        //   this.current = 0;
-        // }
+        if (this.open === false) {
+          this.open = true;
+          this.current = 0;
+        }
       },
 
       // When one of the suggestion is clicked
@@ -74,20 +90,25 @@
         <v-flex xs8 lg6>
           <v-card-text>
             <form>
-              <v-text-field
-                v-model="name"
-                label="Name"
-                :counter="10"
-                :error-messages="errors.collect('name')"
-                v-validate="'required|max:10'"
-                data-vv-name="name"
-                required
-                @keyup.enter = 'enter'
-                @keyup.space = 'enter'
-                @keyup.down = 'down'
-                @keyup.up = 'up'
-                @input = 'change'
-              ></v-text-field>
+              <div style="position:relative" v-bind:class="{'open':openSuggestion}">
+                <v-text-field
+                  v-model="selection"
+                  label="Add Tag"
+                  @keyup.enter.stop = 'enter'
+                  @keyup.space = 'enter'
+                  @keyup.down = 'down'
+                  @keyup.up = 'up'
+                  @input = 'change'
+                ></v-text-field>
+                <ul class="dropdown-menu" style="width:100%">
+                    <li v-for="(s, i) in matches"
+                      :class="{'active': isActive(i)}"
+                      @click="suggestionClick(i)"
+                    >
+                      <a href="#">{{ s }}</a>
+                    </li>
+                </ul>
+              </div>
 
               <v-btn @click="submit">submit</v-btn>
               <v-btn @click="clear">clear</v-btn>
